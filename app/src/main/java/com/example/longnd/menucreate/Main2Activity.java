f8e9d5d6e4007ce801c95a406d5fba13257bf536
long <duclongph1997@gmail.com>
2018-10-14 23:12:10 +0700

    commit 1.2

--- app/src/main/java/com/example/longnd/menucreate/Main2Activity.java
+++ app/src/main/java/com/example/longnd/menucreate/Main2Activity.java
@@ -9,64 +9,81 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Main2Activity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-//    private LinearLayout lay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-        // call NavlgationView with id= tab
-        NavigationView navigationView = findViewById(R.id.tab);
-        navigationView.setNavigationItemSelectedListener(this);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        // ho tro keo menu
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle= new ActionBarDrawerToggle(this, drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-//            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new Default_fragment()).commit();
-//            navigationView.setCheckedItem(R.id.defaultt);
-
+        NavigationView navigationView= findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        if(savedInstanceState==null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new Default_fragment()).commit();
+         }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(actionBarDrawerToggle.onOptionsItemSelected(item))
-        {
-           return  true;
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.START)) {
+            drawerLayout.closeDrawer(Gravity.START);
+        } else {
+            super.onBackPressed();
         }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-        if(menuItem.getItemId()== R.id.logout)
+        if(menuItem.getItemId()== R.id.nav_setting)
         {
-//          getSupportFragmentManager().beginTransaction().replace(R.id.defaultt,new Message_fragment()).commit();
-            Toast.makeText(getApplicationContext(),"log out",Toast.LENGTH_SHORT).show();
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new setting_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
         }
-        if(menuItem.getItemId()== R.id.zoom_in)
+        if(menuItem.getItemId()== R.id.nav_logout)
         {
-            Toast.makeText(getApplicationContext(),"zoom in",Toast.LENGTH_SHORT).show();
-
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new logout_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
         }
-        if(menuItem.getItemId()== R.id.zoom_out)
+        if(menuItem.getItemId()== R.id.nav_send)
         {
-            Toast.makeText(getApplicationContext(),"zoom out ",Toast.LENGTH_SHORT).show();
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new Default_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
         }
-        if(menuItem.getItemId()== R.id.setting)
+        if(menuItem.getItemId()== R.id.nav_share)
         {
-//            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt,new setting_fragment()).commit();
-            Toast.makeText(getApplicationContext(),"setting",Toast.LENGTH_SHORT).show();
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new Default_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
+        }
+        if(menuItem.getItemId()== R.id.nav_zoomin)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new zoom_in_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
+        }
+        if(menuItem.getItemId()== R.id.nav_zoomout)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new zoom_out_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
+        }
+        if(menuItem.getItemId()== R.id.nav_show)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.defaultt, new Message_fragment()).commit();
+            Toast.makeText(getApplicationContext(),"heloo",Toast.LENGTH_LONG);
         }
         drawerLayout.closeDrawer(Gravity.START);
         return true;
